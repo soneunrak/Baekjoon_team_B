4e26224105baf0a92439d2024b496dc690fbf308
soneunrak <128948934+soneunrak@users.noreply.github.com>
2024-02-01 15:42:57 +0900

    [Bronze II] Title: 시험 감독, Time: 640 ms, Memory: 144340 KB -BaekjoonHub

---
+++ 백준/Bronze/13458. 시험 감독/시험 감독.py
@@ -0,0 +1,14 @@
+n = int(input())
+a = list(map(int, input().split()))
+b, c = map(int, input().split())
+
+cnt = n
+for i in a:
+    i -= b
+    if i > 0:
+        if i % c:
+            cnt += i//c + 1
+        else:
+            cnt += i//c
+
+print(cnt)
